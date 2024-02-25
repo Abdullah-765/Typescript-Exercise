a619e41458017bc3dcdf62e5dee012fdf828a6a2
Abdullah-765 <arshadabdullah765@gmail.com>
2024-02-25 10:31:11 +0500

    Completed

---
+++ Que_16/app.js
@@ -0,0 +1,20 @@
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+var guestList = ["Ronaldo", "Babar Azam", "Keanu Reeves", "Khabib"];
+guestList.forEach((function (Element) {
+    return console.log("Hello! ".concat(Element, " how are you? I would like to invite you on dinner, looking forward to your response"));
+}));
+//Exercise 15
+console.log("".concat(guestList[0], " wont be able to make it to dinner maybe next time"));
+var newGuest = "Leanardo Da vinci";
+guestList[0] = newGuest;
+guestList.forEach((function (Element) {
+    return console.log("Hello! ".concat(Element, " how are you? I would like to invite you on dinner, looking forward to your response"));
+}));
+//Exercise 16
+console.log("Great News I just found a bigger table so i can invite more peroples");
+guestList.unshift("Ian Bishop");
+guestList.splice(3, 0, "Sir Zia Khan");
+guestList.push("Virat Kohli");
+guestList.forEach((function (Element) {
+    return console.log("Hello! ".concat(Element, " how are you? I would like to invite you on dinner, looking forward to your response"));
+}));
